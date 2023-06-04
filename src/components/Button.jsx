363e81450e9e76456224e8976163bd7de552c7ab
juanvacag <vacagonzalezjuan@gmail.com>
2023-06-04 21:40:50 +0200

    final

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ function Button({operador, fn}) {
         type='button'
         className='h-10 w-10 flex items-start justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
         onClick={fn}
-  >{operador}</button>  )
+    >{operador}</button>  )
 }
 
 export default Button
