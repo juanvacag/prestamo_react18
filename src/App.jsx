8eaccefe3785768c449a7f41a20bc96ec03463a7
juanvacag <vacagonzalezjuan@gmail.com>
2023-06-04 16:27:30 +0200

    añadiendo botones

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,44 @@ function App() {
     setCantidad(Number(e.target.value))
   }
 
+  function handleClickDecremento() {
+    const valor = cantidad - STEP;
+
+    if(valor < MIN) {
+      alert('Cantidad no valida');
+      return;
+    }
+    setCantidad(valor);
+  }
+
+  
+
+  function handleClickIncremento() {
+    const valor = cantidad + STEP;
+
+    if(valor > MAX) {
+      alert('Cantidad no valida');
+      return;
+    }
+    setCantidad(valor);
+  }
+
   return (
 <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
 
+    <div className='flex justify-between my-6'>
+        <button
+          type='button'
+          className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
+          onClick={handleClickDecremento}
+        >-</button>
+        <button
+          type='button'
+          className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
+          onClick={handleClickIncremento}
+        >+</button>
+    </div>
     <input 
     type='range' 
     className="w-full bg-gray-200 accent-green-500 hover:accent-green-600"
@@ -23,7 +57,11 @@ function App() {
     min={MIN}
     max={MAX}
     step={STEP}
+    value={cantidad}
     />
+    <div className='flex justify-between my-14'>
+        
+    </div>
 
     <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>{cantidad}</p>
 </div>
